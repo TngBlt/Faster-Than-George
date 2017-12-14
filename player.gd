b06a0f1b35275941b65e9bc48d5b734093a27699
Tanguy <tanguy.blochet@viacesi.fr>
2017-12-14 16:35:18 +0100

    FTGv2

--- player.gd
+++ player.gd
@@ -1,62 +1,25 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-var input_direction = 0
-var direction = 0
+var btn_right = Input.is_action_pressed("ui_right")
+var btn_left = Input.is_action_pressed("ui_left")
+export var speed = 200
+export var acceleration = 5
+var current_speed = Vector2(0,0)
 
-var speed_x = 0
-var speed_y = 0
-var velocity = Vector2()
-const MAX_SPEED = 600
-const ACCELERATION = 600
-const DECELERATION = 600
-
-const JUMP_FORCE = 800
-const GRAVITY = 2000
+func move(speed, acc, delta):
+	current_speed.x =lerp(current_speed.x, speed, acc*delta)
+	set_linear_velocity(Vector2(current_speed.x,get_linear_velocity().y))
 
 func _ready():
-	set_process(true)
-	set_process_input(true)
+	set_fixed_process(true)
 	
-func _input(event):
-	if event.is_action_pressed("jump"):
-		speed_y = -JUMP_FORCE
-	pass
-
-func _process(delta):
-	if (is_colliding()):
-		print("COLLIDING WITH", get_collider())
-		
-	 # INPUT
-	if input_direction:
-		direction = input_direction
-   
-	if Input.is_action_pressed("move_left"):
-		input_direction = -1
-	elif Input.is_action_pressed("move_right"):
-		input_direction = 1
+func _fixed_process(delta):
+	btn_right = Input.is_action_pressed("ui_right")
+	btn_left = Input.is_action_pressed("ui_left")
+	
+	if btn_left:
+		move(-speed, acceleration,delta)
+	elif btn_right:
+		move(speed, acceleration,delta)
 	else:
-		input_direction = 0
-	
-	# MOVEMENT
-	if input_direction == - direction:
-		speed_x /= 3
-	if input_direction:
-		speed_x += ACCELERATION * delta
-	else:
-		speed_x -= DECELERATION * delta
-	speed_x = clamp(speed_x, 0, MAX_SPEED)
-	
-	speed_y += GRAVITY * delta
-		
-	velocity.x = speed_x * delta * direction
-	velocity.y = speed_y * delta
-	move(velocity) 
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		move(0, acceleration,delta)
