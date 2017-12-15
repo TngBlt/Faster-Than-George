cc98d017d71a182f74a1f2ba8ec03931a309f76a
AdrienKegler <adrien.kegler@viacesi.fr>
2017-12-15 09:18:58 +0100

    Version sans les bugs en cadeau
    retrait des milkyways et du script papa World.gd

--- FTGv2/MilkyWay.gd
+++ FTGv2/MilkyWay.gd
@@ -5,7 +5,7 @@ export var value = 1
 
 func _ready():
 	if get_owner() != null:
-d		get_owner().NbTotalMilkyWay += value
+		get_owner().NbTotalMilkyWay += value
 	get_node("Area2D").connect("body_enter", self, "_collectMilkyWay")
 
 
